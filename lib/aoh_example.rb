394d2d599c247c8d764bc8abf550b1eaaf467f12
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-21 20:02:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,6 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   characters = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
-  characters
 end
 
 def aoh_lookup(aoh, row, key)
